087abc6485b7b7dd71b499f9491d8861e5c363c3
Ikaguia <ikaguia@github.com>
2019-11-08 13:02:46 -0300

    Memória::AlocaBloco

--- include/memoria.hpp
+++ include/memoria.hpp
@@ -3,6 +3,11 @@
 #ifndef MODULO_MEMORIA
 #define MODULO_MEMORIA
 
-
+namespace Memoria{
+	enum Tipo {TEMPO_REAL = 0, USUARIO};
+	extern list<pair<int, int>> mem[2];
+	void Init();
+	int AlocaBloco(int tamanho, Tipo tipo, int pid);
+};
 
 #endif//MODULO_MEMORIA
--- src/memoria.cpp
+++ src/memoria.cpp
@@ -1,2 +1,24 @@
 #include <memoria.hpp>
 
+namespace Memoria{
+	list<pair<int, int>> mem[2];
+	void Init(){
+		mem[TEMPO_REAL].emplace_back(64, -1);
+		mem[USUARIO].emplace_back(960, -1);
+	}
+	int AlocaBloco(int tamanho, Tipo tipo, int pid){
+		int posicao = 0;
+		for(pair<int, int> &bloco : mem[tipo]){
+			if(bloco.first >= tamanho and bloco.second == -1){
+				if(bloco.first == tamanho) bloco.second = pid;
+				else{
+					// TODO
+				}
+				return posicao;
+			}
+			posicao += bloco.first;
+		}
+		return -1;
+	}
+};
+
