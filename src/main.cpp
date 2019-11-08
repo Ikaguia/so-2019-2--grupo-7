32b2721f82f72fcf974d04a19de0bc2eba0cda9c
maffei2443 <leoitu22hotmail.com@gmail.com>
2019-11-08 12:49:16 -0300

    padronizando nomenclatura

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <common.hpp>
-#include <filas.hpp>
-#include <processos.hpp>
+#include <Filas.hpp>
+#include <Processos.hpp>
 
 int main(int argc, char **argv){
 
@@ -8,35 +8,35 @@ int main(int argc, char **argv){
 	if(not file_exists(argv[1])) return cout << "ERRO: Arquivo invalido '" << string(argv[1]) << "'\n", 1;
 	if(not file_exists(argv[2])) return cout << "ERRO: Arquivo invalido '" << string(argv[2]) << "'\n", 1;
 
-	processos::le_arquivo(argv[1]);
+	Processos::le_arquivo(argv[1]);
 
-	int processo_atual = -1;
+	int Processo_atual = -1;
 	queue<int> *fila_retro = nullptr;
 
 	FOR(tempo, 0, 1000000000){
-		//verifica se tem processos que são inicializados no tempo atual
-		processos::adiciona(tempo);
-		if(processo_atual == -1){//se o ultimo processo foi interrompido
-			//processo a ser executado, fila onde ele vai ser retroalimentado
-			tie(processo_atual, fila_retro) = filas::proximo();
+		//verifica se tem Processos que são inicializados no tempo atual
+		Processos::adiciona(tempo);
+		if(Processo_atual == -1){//se o ultimo Processo foi interrompido
+			//Processo a ser executado, fila onde ele vai ser retroalimentado
+			tie(Processo_atual, fila_retro) = Filas::proximo();
 		}
-		if(processo_atual == -1){//não tem processo na fila de prontos
-			if(processos::pq.empty()) break;//não tem processos com tempo de inicialização maior que o atual
+		if(Processo_atual == -1){//não tem Processo na fila de prontos
+			if(Processos::pq.empty()) break;//não tem Processos com tempo de inicialização maior que o atual
 			else cout << tempo << " IDLE\n";
 		}
 		else{
 			cout << tempo << " ";
-			//executa o processo atual, e verifica se ele terminuou
-			bool acabou = processos::proc[processo_atual].executa();
+			//executa o Processo atual, e verifica se ele terminuou
+			bool acabou = Processos::proc[Processo_atual].executa();
 			if(not acabou){
-				if(processos::proc[processo_atual].prioridade != 0){ // Proc. de usuario deve ser preemptado
-					fila_retro->push(processo_atual); //adiciona ele na sua fila de retroalimentação
-					processo_atual = -1;
+				if(Processos::proc[Processo_atual].prioridade != 0){ // Proc. de usuario deve ser preemptado
+					fila_retro->push(Processo_atual); //adiciona ele na sua fila de retroalimentação
+					Processo_atual = -1;
 					fila_retro = nullptr;
 				}
 			}
-			else{ //se o processo acabou
-				processo_atual = -1;
+			else{ //se o Processo acabou
+				Processo_atual = -1;
 				fila_retro = nullptr;
 			}
 		}
